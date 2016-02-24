8bd5d818480d293ba72d16bf4d36391f38dd1247
Sergio Fernández <fdz.sergio@gmail.com>
2016-02-24 20:44:12 +0100

    fixing example exec on device

---
+++ Example/Example/SFFocusViewLayout.framework/Headers/SFFocusViewLayout.h
@@ -0,0 +1,19 @@
+/**
+ This file is part of the SFFocusViewLayout package.
+ (c) Sergio Fernández <fdz.sergio@gmail.com>
+
+ For the full copyright and license information, please view the LICENSE
+ file that was distributed with this source code.
+ */
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SFFocusViewLayout.
+FOUNDATION_EXPORT double SFFocusViewLayoutVersionNumber;
+
+//! Project version string for SFFocusViewLayout.
+FOUNDATION_EXPORT const unsigned char SFFocusViewLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SFFocusViewLayout/PublicHeader.h>
+
+
