ae15ca5af8e235ccbe565055bfc3c30eee29b372
Sergio Fernández <fdz.sergio@gmail.com>
2016-11-01 18:23:01 +0100

    add spm

---
+++ Package.swift
@@ -0,0 +1,13 @@
+/**
+ This file is part of the SFFocusViewLayout package.
+ (c) Sergio Fernández <fdz.sergio@gmail.com>
+
+ For the full copyright and license information, please view the LICENSE
+ file that was distributed with this source code.
+*/
+
+import PackageDescription
+
+let package = Package(
+  name: "SFFocusViewLayout"
+)
